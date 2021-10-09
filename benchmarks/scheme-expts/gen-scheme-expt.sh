6e15df9bcba2c846be7f571edbae930d88f33e2f
Kristopher Micinski <krismicinski@gmail.com>
2021-10-08 20:15:16 -0400

    adding

--- benchmarks/scheme-expts/gen-scheme-expt.sh
+++ benchmarks/scheme-expts/gen-scheme-expt.sh
@@ -7,6 +7,7 @@ program=$1
 here=$(pwd)
 fact_dir=souffle-facts-$program
 slog_dir=$(pwd)/$1-slog
+slog_input=$slog_dir/input
 mkdir $fact_dir &>/dev/null
 output_exe=souffle-compiled-$program
 cat scheme-mcfa.slog >$program.slog
@@ -14,11 +15,13 @@ racket scheme-fact-gen.rkt $2 $fact_dir >>$program.slog
 echo "Compiling Souffle source using $3 cores"
 souffle -c -j$3 -o $output_exe scheme_mcfa.dl -F $fact_dir &>/dev/null
 echo "Compiling Slog source to C++ (output db in $slog_dir)"
-cd ../../compiler; racket slog.rkt -o -c --output-db $slog_dir $here/scheme-mcfa.slog
+rm -rf ../../data/scheme-mcfa-input
+cd ../../compiler; racket slog.rkt -o -c --output-db $slog_dir $here/$program.slog
 cd $here
+cp -r ../../data/$program-input $slog_input
 cp CMakeLists.txt $slog_dir
-sed 's/..\/src\/parallel_RA_inc.h/..\/..\/..\/backend\/src\/parallel_RA_inc.h/g' $slog_dir/scheme-mcfa.cpp >$slog_dir/target.cpp
-mv $slog_dir/target.cpp $slog_dir/scheme-mcfa.cpp
+sed 's/..\/src\/parallel_RA_inc.h/..\/..\/..\/backend\/src\/parallel_RA_inc.h/g' $slog_dir/$program.cpp >$slog_dir/target.cpp
+mv $slog_dir/target.cpp $slog_dir/$program.cpp
 cd $slog_dir
 echo "Compiling Slog C++ -> Binary"
 cmake . &>/dev/null
