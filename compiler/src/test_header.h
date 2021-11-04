a69de225c293f1a57141045908a49e834c1b33f1
Yihao Sun <stargazermiao@gmail.com>
2021-11-04 11:07:12 -0400

    add more test

--- compiler/src/test_header.h
+++ compiler/src/test_header.h
@@ -1,6 +1,7 @@
 
-using u64 = unsigned long;
+#define u64 unsigned long
 using local_agg_res_t = u64;
+
 // struct _BTree {
 //   virtual bool has_key(const u64* key) = 0;
 // };
@@ -9,4 +10,4 @@ typedef local_agg_res_t *local_agg_func_t (shmap_relation* agg_rel, const u64* d
 
 typedef local_agg_res_t *reduce_agg_func_t (local_agg_res_t x, local_agg_res_t y);
 
-typedef int *global_agg_func_t (u64* data, local_agg_res_t agg_data, int agg_data_count, u64* output); 
+typedef int *global_agg_func_t (u64* data, local_agg_res_t agg_data, int agg_data_count, u64* output);
