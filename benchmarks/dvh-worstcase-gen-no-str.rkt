09ec60b91bdb10222284a98dbb88fc74bcafe412
Tom <tom@pop-os.localdomain>
2021-11-17 12:52:01 -0600

    modest and two dvh-worstcase generators, one using strings one using numbers

---
+++ benchmarks/dvh-worstcase-gen-no-str.rkt
@@ -0,0 +1,25 @@
+#lang racket
+
+(define (generate-term t k [i 0])
+  (define (generate-clo-app j)
+    (if (= j 0)
+	`(app (ref 2) (ref 4))
+	`(app ,(generate-clo-app (- j 1)) (ref ,(+ 4 j)))))
+  (define (generate-padding k)
+    (if (= k 0)
+	`(lam 0 ,(generate-clo-app (- t 1)))
+	`(app (lam 3 ,(generate-padding (- k 1))) (lam 3 (ref 3)))))
+  (define fx (+ 1000 i))
+  (define zx (+ 4 i))
+  (if (>= i t)
+      (generate-padding k)
+      `(app
+	(lam ,fx
+	     (seq (app (ref ,fx) (int 0))
+		  (app (ref ,fx) (int 1))))
+	(lam ,zx
+	     ,(generate-term t k (+ i 1))))))
+
+(pretty-print
+ `(entry-point
+   ,(generate-term 2 3)))
