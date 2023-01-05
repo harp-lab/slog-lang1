1c8f027009af2699aab17c56385ace15f4ff6f0d
ysun67 <ysun67@its-rc-kkmicins.ad.syr.edu>
2023-01-05 12:46:37 -0500

    stage change

--- backend/src/lie/lie.cpp
+++ backend/src/lie/lie.cpp
@@ -433,9 +433,7 @@ bool LIE::execute ()
 #endif
 
         // load balance before a SCC executed
-        if (executable_task->balance_flag) {
         executable_task->load_balance();
-        }
 
         if (restart_flag == false)
         {
