4174d2365c5fc6031908eae8e89276acad766d20
Michael Gathara <mikegtrm@gmail.com>
2023-08-02 22:21:51 -0500

    runslog changes for enterrepl, better printing (#41)
    * runslog changes for enterrepl, better printing
    
    * this should fix the CI error
    
    * CI test for validation
    
    * slog file and out dir are optional non tagged args
    
    * usage text change
    
    * completer exit fix

--- slog/repl/repl.py
+++ slog/repl/repl.py
@@ -65,14 +65,15 @@ HELP_RUNSLOG = '''
     help                print help info
     dump <rel>          dump all facts inside a relation
     relations           print all relation meta info in current database
+    find                find facts containing a specific piece of text
 '''
 
 CMD = ['help', 'run', 'connect', 'dump', 'showdb', 'relations',
        'load', 'compile', 'tag', 'switch', 'fact-depth',
-       'fact-cardi', 'clear', 'fresh']
+       'fact-cardi', 'clear', 'fresh', 'find', 'exit']
 
 # in run slog CLI, you can only do fact dumping, we need better pagenation
-CMD_RUNSLOG = ['help',  'dump', 'relations']
+CMD_RUNSLOG = ['help',  'dump', 'relations', 'find', 'exit']
 
 kb = KeyBindings()
 
@@ -84,9 +85,20 @@ def _(event):
 def _(event):
     event.current_buffer.validate_and_handle()
 
+class colors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 def invalid_alert(message):
     """ print alert for command exectution """
-    print(f"Invalid command: {message}")
+    print(f"{colors.FAIL}Invalid command: {colors.ENDC}{message}")
 
 def prompt_continuation(width, line_number, is_soft_wrap):
     return '.' * width
@@ -103,7 +115,7 @@ def exec_command(client: SlogClient, raw_input: str):
     # check if it a query
     if (raw_input.startswith('(') and raw_input.endswith(')')) or \
        (raw_input.startswith('[') and raw_input.endswith(']')):
-        client.slog_add_rule(raw_input, client.cur_db, ConsoleWriter())
+        client.slog_add_rule(raw_input[1:], client.cur_db, ConsoleWriter())
         return
     if raw_input.startswith('?(') and raw_input.endswith(')'):
         client.pretty_print_slog_query(raw_input, ConsoleWriter())
@@ -154,6 +166,15 @@ def exec_command(client: SlogClient, raw_input: str):
                 invalid_alert(f'{cmd} expect a string at postion 2 as arg')
         else:
             invalid_alert(f'{cmd} expect 1/2 arg, but get {len(args)}')
+    elif cmd == 'find':
+        if len(args) == 0:
+            invalid_alert(f'{cmd} expects a string at position 1 as arg')
+        elif args[0].startswith('"') and args[0].endswith('"'):
+            print(args[0][1:-1])
+            client.find(args[0][1:-1], ConsoleWriter())
+        else:
+            client.find(" ".join(args))
+            # invalid_alert(f'{cmd} ran into some random error')
     elif cmd == 'fact-depth':
         if len(args) == 1 and args[0].isnumeric():
             client.unroll_depth = int(args[0])
@@ -212,6 +233,8 @@ def exec_command(client: SlogClient, raw_input: str):
             client.switchto_db(args[0])
         else:
             invalid_alert(f'{cmd} expected 1 argument, got {len(args)}.')
+    elif cmd == 'exit':
+        client.exit()
     else:
         invalid_alert(f'{cmd} is not a valid command, type `help to see help`')
 
@@ -263,10 +286,10 @@ class Repl:
         else:
             return HTML('Disconnected. Use `connect <host>`')
 
-    def exit(self):
-        """ exit REPL """
-        print('Goodbye.')
-        sys.exit(0)
+    # def exit(self):
+    #     """ exit REPL """
+    #     print('See you again')
+    #     sys.exit(0)
 
     def loop(self):
         """  REPL main entrance """
@@ -295,6 +318,7 @@ class Repl:
                     completer_map['load'] = StringPathCompeleter()
                     completer_map['compile'] = StringPathCompeleter()
                     completer_map['switch'] = FuzzyWordCompleter(possible_db_hash + possible_db_tag)
+                    # completer_map['exit'] = repl.exit()
                     relname_par_completer = WordCompleter([f"({n}" for n in relation_names])
                     for rname in relation_names:
                         completer_map[f'?({rname}'] = relname_par_completer
@@ -312,6 +336,7 @@ class Repl:
                     continue
                 prev_time = time.time()
                 exec_command(self.client, text)
+            
                 after_time = time.time()
                 print("\033[93mCommand cost {:10.2f} sec.\033[4m".format(after_time - prev_time)) 
             except EOFError:
